b878c2203c632926088593ad1c4c79019eb38d7f
gormanucharyan <gor.manucharyan2003@gmail.com>
2021-10-20 22:50:56 +0400

    commit

--- task3_4.js
+++ task3_4.js
@@ -1,6 +1,6 @@
 let x1 = 4;
 let y1 = 5;
-let x2 = 3;
+let x2 = 16;
 let y2 = 8;
 let distance = Math.sqrt((x1-x2)**2 + (y1 -y2)**2);
 console.log("The length of the line from "+ "( "+ x1 + "," + y1 + " ) " + " to " + "( "+ x2 + "," + y2 + " ) " + " is " + distance);
