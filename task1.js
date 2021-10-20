0c38ff0af4b319d15a8ab91af8bda35c8d2551e3
gormanucharyan <gor.manucharyan2003@gmail.com>
2021-10-20 19:52:49 +0400

    first commit

---
+++ task1.js
@@ -0,0 +1,15 @@
+let height = 8;
+let base = 10; 
+let width = 5;
+let length = 14;
+let traingleArea = height * base / 2;
+let rectangleArea = width * length;
+if (rectangleArea > traingleArea){
+    console.log("The rectangle has a bigger area");
+}
+else if (traingleArea > rectangleArea){
+    console.log("The triangle has a bigger area");
+}
+else{
+    console.log("The triangle and rectangle have the same area");
+}
